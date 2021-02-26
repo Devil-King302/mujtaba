abf7111d880908bcf8bb4338a41edfcfd5d938ab
Mujoo-Khan <79643352+Mujoo-Khan@users.noreply.github.com>
2021-02-26 16:00:05 +0500

    Update mujtaba.py

--- mujtaba.py
+++ mujtaba.py
@@ -51,15 +51,15 @@ logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
 |__(~)    •||•THE - Mujoo - KinG------•◈•
 |__\~~) •||•Mujoo - Khan ---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(-----\  •◈•------MUJOO - KHAN --------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMujoo Khan
+\033[1;96mInstagram \033[1;93m: \033[1;Mujookhan007
+\033[1;96mFacebook  \033[1;93m: \033[1; Mujoo Khan
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/mujookhan/mujtaba
 \033[1;91m======================================="""
 
 def tik():
@@ -80,10 +80,10 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMujoo khan
+\033[1;96mInstagram \033[1;93m: \033[1;92mmujookhan007
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Mujoo Khan
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Mujookhan/mujtaba
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system
 
 def login():
 	os.system('clear')
